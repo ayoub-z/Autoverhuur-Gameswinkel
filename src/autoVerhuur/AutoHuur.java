7c2f897b741f25d0d62a979b71ec253de13bcbe8
ayoub-z <60653320+ayoub-z@users.noreply.github.com>
2021-04-18 14:29:39 +0200

    Added toStrings, main() + fixed totaalPrijs func

--- src/autoVerhuur/AutoHuur.java
+++ src/autoVerhuur/AutoHuur.java
@@ -19,7 +19,7 @@ class Klant{
 	
 	// print Klant
 	public String toString(){
-		return "Op naam van: " + naam;		
+		return "Op naam van: " + naam + " (Korting: " + kortingsPercentage + "%)";		
 	}
 }
 
@@ -45,7 +45,7 @@ class Auto{
 	
 	// print Auto
 	public String toString(){
-		return null;		
+		return "autotype: " + type + " met prijs per dag: " + prijsPerDag;		
 	}
 }
 
@@ -80,8 +80,8 @@ public class AutoHuur{
 	}	
 
 	// getter for rented Auto
-	public Auto getGehuurdeAuto(Auto Auto){
-		return Auto;
+	public String getGehuurdeAuto(){
+		return gehuurdeAuto.toString();
 	}
 	
 	// getter for Huurder
@@ -91,21 +91,49 @@ public class AutoHuur{
 
 	public double totaalPrijs(){
 		this.totaalPrijs = aantalDagen * gehuurdeAuto.getPrijsPerDag();
+		this.totaalPrijs = totaalPrijs - (totaalPrijs * huurder.getKorting() / 100);
+		
 		return this.totaalPrijs;
 	}
 
 	// print AutoHuur
 	public String toString(){
-		return "Aantal dagen: " + aantalDagen + " en dat kost: " + totaalPrijs;
+		if (this.huurder == null && this.gehuurdeAuto == null){
+			return "\ter is geen auto bekend" + "\n\tEr is geen huurder bekend" +
+			"\n\tAantal dagen: " + aantalDagen + " en dat kost: " + totaalPrijs;
+		}
+		else if (this.huurder != null && this.gehuurdeAuto == null){
+			return "\tAantal dagen: " + aantalDagen + " en dat kost: " + totaalPrijs() +
+			"\n" + huurder.toString() + "\n\n" + this.gehuurdeAuto;			
+		}
+		else{ 
+			return "\t" + gehuurdeAuto.toString() + "\n\t" + huurder.toString() + 
+			"\n\tAantal dagen: " + aantalDagen + " en dat kost: " + totaalPrijs();
+			
+		}
 	}
 	public static void main(String[] args){
 		AutoHuur ah1 = new AutoHuur();
-		System.out.println("Eerste autohuur: " + ah1.toString());
+		AutoHuur ah2 = new AutoHuur();
+		System.out.println("\nEerste autohuur: \n" + ah1.toString());
 
 		Klant k = new Klant("Mijnheer de vries");
 		k.setKorting(10.0);
 		ah1.setHuurder(k);
-		System.out.println(ah1.toString());
+		Auto a1 = new Auto("Peugot 207", 50.0);
+		ah1.setGehuurdeAuto(a1);
+		ah1.setAantalDagen(4);
+
+		Auto a2 = new Auto("Ferrari", 3500);
+		ah2.setGehuurdeAuto(a2);
+		ah2.setHuurder(k);
+		ah2.setAantalDagen(1);
+
+		System.out.println("\nEerste autohuur: \n" + ah1.toString());
+		System.out.println("\nTweede autohuur: \n" + ah2.toString());
+
+		System.out.println("Gehuurd: " + ah1.getGehuurdeAuto());
+		System.out.println("Gehuurd: " + ah2.getGehuurdeAuto());		
 	}
 }
 
